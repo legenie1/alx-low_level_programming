1cabec06b390ed49e2dcada2d8622b46545097ab
legenie1 <sergeomoffo1@gmail.com>
2022-03-16 21:45:46 +0100

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-*print_alphabet_10 - prints the alphabet 10 times
-*You can only use putchar twice
-*Return - nothing
-*/
+ * print_alphabet_x10 - prints the alphabet 10 times
+ * You can only use putchar twice
+ * Return - nothing
+ */
 
 void print_alphabet_x10(void)
 {
 	int l;
 	int i = 0;
 
-	while(i < 10)
+	while (i < 10)
 	{
 		l = 'a';
 		while (l <= 'z')
