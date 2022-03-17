a3015961745a6309cc69a4db69be9fc4102a11c8
legenie1 <sergeomoffo1@gmail.com>
2022-03-17 13:04:38 +0100

    new

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,11 +24,23 @@ else if (b > a && b > c)
 {
 largest = b;
 }
+else if (b > c && c > a)
+{
+largest = b;
+}
 else if (a > b && a > c)
 {
 largest = a;
 }
+else if (a > c && c > b)
+{
+largest = a;
+}
 else if (c > b && b > a)
+{
+largest = c;
+}
+else if (c > a && a > b)
 {
 largest = c;
 }
