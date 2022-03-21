af5a1403d5584327e28736112b15571a6c8a6ddd
legenie1 <sergeomoffo1@gmail.com>
2022-03-21 21:43:45 +0100

    Exercise on pointers and array

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * _puts - function that prints a string
+ * puts2 - function that prints every other character
  *
  * @str: pointer to char
  * Return: nothing
  */
 
-void _puts(char *str)
+void puts2(char *str)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
 		i += 1;
 	}
 	_putchar('\n');
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#This part is to learn more about pointers and arrays
+It's made up of 9 mandatory Task
