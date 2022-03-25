4a67fd3bc80fbf6fc75078a02286cdc694abeaac
legenie1 <sergeomoffo1@gmail.com>
2022-03-25 01:37:18 +0100

    More excercise on pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat- concatenates string with specific memry size
+ * @dest: pointer on type char
+ * @src: pointer on type char
+ * @n: integer for byte size
+ * Return: char
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+	while (*(src + j) != '\0')
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	return (dest);
+}
