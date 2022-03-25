ec78b14caf89d9c69c3119f76012e3e9f5b98fbc
legenie1 <sergeomoffo1@gmail.com>
2022-03-25 01:53:31 +0100

    More excercise on pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy- this function copies a string
+ * @dest: pointer on char
+ * @src: pointer on char
+ * @n: number of bytes received
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && *(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i != n)
+		dest[i++] = '\0';
+	return (dest);
+}
