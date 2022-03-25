d7dce2902fe3b958af23f616d48991c964a2bfd3
legenie1 <sergeomoffo1@gmail.com>
2022-03-25 01:18:05 +0100

    More excercise on pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,18 +9,19 @@
 
 int _strcat(char *dest, char *src))
 {
-	while (*dest != '\0')
+	int i = 0;
+	int j = 0;
+
+	while (*(dest + i) != '\0')
 	{
-		dest++;
+		i++;
 	}
-	while (*src != '\0')
+	while (*(src + j) != '\0')
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
 	}
-	*dest = '\0';
-	_putchar(dest);
 
 	return (dest);
 }
