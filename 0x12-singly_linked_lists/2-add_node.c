c411bc4ad787c93c53aa3c30ff0e6d44592aee51
legenie1 <sergeomoffo1@gmail.com>
2022-04-23 02:57:25 +0100

    Singly Linked List Project

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,33 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
+#include <string.h>
+
 /**
- * add_node - add a new node at the beginning of a list
- * @head: head of the linked list
- * @str: string in the struct
- *
- * Return: the address of the new element
+ * add_node - function that prints all the elements of a list_t list
+ * @head: input header pointer
+ * @str: Input string value
+ * Return: address of the new element, or NULL if it failed
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newnode;
-	int newelem = 0;
+	list_t *new_node;
 
-	newnode = malloc(sizeof(list_t));
-
-	if (newnode == NULL)
-	{
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
-	}
+	new_node->len = _strlen(str);
+	new_node->str = strdup(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
 
-	newnode->str = strdup(str);
+/**
+ * _strlen - returns length of string
+ * @s: character of string
+ * Return: length of string
+ */
 
-	while (str[newelem])
-	{
-		newelem++;
-	}
+int _strlen(const char *s)
+{
+	int i;
 
-	newnode->len = newelem;
-	newnode->next = *head;
-	*head = newnode;
-	return (*head);
+	while (s[i] != 0)
+		i++;
+	return (i);
 }
 
