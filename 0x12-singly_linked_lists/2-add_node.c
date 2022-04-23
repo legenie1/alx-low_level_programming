40bc20c65570ebccbc706d760530b067c7a9fc66
legenie1 <sergeomoffo1@gmail.com>
2022-04-23 02:52:36 +0100

    Singly Linked List Project

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,32 @@
 #include "lists.h"
-#include <string.h>
-
 /**
- * add_node - Adds a new node at the beginning
- *            of a list_t list.
- * @head: A pointer to the head of the list_t list.
- * @str: The string to be added to the list_t list.
+ * add_node - add a new node at the beginning of a list
+ * @head: head of the linked list
+ * @str: string in the struct
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new element.
+ * Return: the address of the new element
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *new;
+	list_t *newnode;
+	int newelem = 0;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+	newnode = malloc(sizeof(list_t));
 
-	dup = strdup(str);
-	if (dup == NULL)
+	if (newnode == NULL)
 	{
-		free(new);
 		return (NULL);
 	}
 
-	for (len = 0; str[len];)
-		len++;
+	newnode->str = strdup(str);
 
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
+	while (str[newelem])
+	{
+		newelem++;
+	}
 
-	*head = new;
-
-	return (new);
+	newnode->len = newelem;
+	newnode->next = *head;
+	*head = newnode;
+	return (*head);
 }
