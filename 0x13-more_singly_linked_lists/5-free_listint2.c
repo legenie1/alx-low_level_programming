a3dbb50a1f56ff08ce5b3b3340f5dff82cb144cd
legenie1 <sergeomoffo1@gmail.com>
2022-04-26 09:46:20 +0100

    More singly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,9 @@
-/*
- * File: 5-free_listint2.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
 
 /**
  * free_listint2 - Frees a listint_t list.
  * @head: A pointer to the address of the
  *        head of the listint_t list.
- *
  * Description: Sets the head to NULL.
  */
 void free_listint2(listint_t **head)
@@ -28,3 +22,4 @@ void free_listint2(listint_t **head)
 
 	head = NULL;
 }
+
